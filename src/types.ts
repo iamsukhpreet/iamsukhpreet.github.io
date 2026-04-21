dca0bd3b354a4355297e2510a0c19a4cdc8a97b3
Ranjodh-Singh <ranjodh.87@gmail.com>
2026-04-21 22:38:49 +0530

    fix: resolve missing types and incorrect component imports

---
+++ src/types.ts
@@ -0,0 +1,52 @@
+export type Site = {
+  website: string;
+  author: string;
+  profile: string;
+  desc: string;
+  title: string;
+  ogImage?: string;
+  lightAndDarkMode: boolean;
+  postPerIndex: number;
+  postPerPage: number;
+  scheduledPostMargin: number;
+  showArchives?: boolean;
+  showBackButton?: boolean;
+  editPost?: {
+    enabled: boolean;
+    text: string;
+    url: string;
+  };
+  dynamicOgImage?: boolean;
+  dir?: string;
+  lang?: string;
+  timezone?: string;
+};
+
+export type SocialObjects = {
+  name: SocialMedia;
+  href: string;
+  active: boolean;
+  linkTitle: string;
+}[];
+
+export type SocialMedia =
+  | "GitHub"
+  | "Instagram"
+  | "LinkedIn"
+  | "Mail"
+  | "Twitter"
+  | "Twitch"
+  | "Facebook"
+  | "WhatsApp"
+  | "Snapchat"
+  | "TikTok"
+  | "Codepen"
+  | "Discord"
+  | "GitLab"
+  | "Reddit"
+  | "Skype"
+  | "Steam"
+  | "Telegram"
+  | "Mastodon"
+  | "Pinterest"
+  | "X";
